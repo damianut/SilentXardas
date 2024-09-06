d2667b4198a2772e157279601e0774bb78fc01e8
damianut <damianas1999@gmail.com>
2024-09-06 16:07:00 +0200

    Version 1.02
    - try to omit validation

--- Ninja/SilentXardas/Content/DIA_NONE_100_Xardas.d
+++ Ninja/SilentXardas/Content/DIA_NONE_100_Xardas.d
@@ -1,8 +1,12 @@
 // Start the first dialogue only, when Xardas is in ZS_Talk, so when player clicked him.
 func int Ninja_SilentXardas_DIA_Xardas_Hello_Condition()
 {
-    if (true == Npc_IsInState(self, ZS_TALK))
-    {
+    if (MEM_FindParserSymbol("ZS_TALK") != -1) {
+        if (true == Npc_IsInState(self, ZS_TALK))
+        {
+            return TRUE;
+        };
+    } else {
         return TRUE;
     };
 };
